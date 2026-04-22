a2cc37b13733f8286ec27bd68ca1cb86348b8ede
2iaad <ziadderfoufi77@gmail.com>
2026-04-22 13:50:21 +0100

    [feat] Majority Element

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,19 +1,24 @@
+/**
+ * this is solved using Boyer-Moore Majority Voting Algorithm
+ */
 class MajorityElement {
     public int majorityElement(int[] nums) {
-        int majority = nums.length / 2;
-        int candidate = nums[0];
+        int candidate = 0;
         int votes = 0;
         for (int num : nums)
         {
+            if (votes == 0)
+                candidate = num;
             if (candidate == num)
                 votes++;
-            if (votes > majority)
-                return candidate;
+            else 
+                votes--;
         }
         return candidate;
     }
     public void main(String[] args) {
         int[] arr = {2,2,1,1,1,2,2};
-        majorityElement(arr);
+        int a = majorityElement(arr);
+        System.out.println(a);
     }
 }
